624305cc30b4c0a2c190750c00a8d2aa050b6279
timothoms <timo@geeky.net>
2021-06-18 13:58:50 -0400

    website reorg

--- setup.R
+++ setup.R
@@ -1,9 +1,11 @@
 library(tidyverse)
 knitr::knit_hooks$set(webgl = rgl::hook_webgl)
-knitr::knit_hooks$set(optipng = knitr::hook_optipng)
-knitr::knit_hooks$set(pngquant = knitr::hook_pngquant)
-if(params$optimize) knitr::opts_chunk$set(optipng = "-o7")
-knitr::opts_chunk$set(echo = TRUE, cache = FALSE,
+if(params$optimize) {
+  knitr::knit_hooks$set(optipng = knitr::hook_optipng)
+  knitr::knit_hooks$set(pngquant = knitr::hook_pngquant)
+  knitr::opts_chunk$set(optipng = "-o7")
+}
+knitr::opts_chunk$set(echo = TRUE, cache = params$cache,
                       fig.width = 7, fig.height = 5,
                       out.width = "67%", out.height = "67%",
                       rownames.print = FALSE,
