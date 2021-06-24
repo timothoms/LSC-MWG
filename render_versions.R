5aff9ca20c5b1caf656fdb8e139e1145f3d29362
timothoms <timo@geeky.net>
2021-06-24 10:03:37 -0400

    fixed params issue

--- render_versions.R
+++ render_versions.R
@@ -1,17 +1,17 @@
 rmarkdown::render_site('/Users/oskarntthoms/Documents/GitHub/LSC-MWG/panel.Rmd')
 
-rmarkdown::render('/Users/oskarntthoms/Documents/GitHub/LSC-MWG/panel.Rmd', output_dir = "docs", envir = new.env(),
+rmarkdown::render('/Users/oskarntthoms/Documents/GitHub/LSC-MWG/panel.Rmd', output_dir = "docs",
                   output_file = "panel1995.html",
                   params = list(set_title = "Panel Regressions (1995-2015)", cache = FALSE, begin = 1995, end = 2015, incl_new = FALSE))
 
-rmarkdown::render('/Users/oskarntthoms/Documents/GitHub/LSC-MWG/panel.Rmd', output_dir = "docs", envir = new.env(),
+rmarkdown::render('/Users/oskarntthoms/Documents/GitHub/LSC-MWG/panel.Rmd', output_dir = "docs",
                   output_file = "panel2lags.html",
                   params = list(set_title = "Panel Regressions (2 lags)", cache = FALSE, lags = 2, incl_new = FALSE))
 
-rmarkdown::render('/Users/oskarntthoms/Documents/GitHub/LSC-MWG/panel.Rmd', output_dir = "docs", envir = new.env(),
+rmarkdown::render('/Users/oskarntthoms/Documents/GitHub/LSC-MWG/panel.Rmd', output_dir = "docs",
                   output_file = "panel3lags.html",
                   params = list(set_title = "Panel Regressions (3 lags)", cache = FALSE, lags = 3, incl_new = FALSE))
 
-rmarkdown::render('/Users/oskarntthoms/Documents/GitHub/LSC-MWG/panel.Rmd', output_dir = "docs", envir = new.env(),
+rmarkdown::render('/Users/oskarntthoms/Documents/GitHub/LSC-MWG/panel.Rmd', output_dir = "docs",
                   output_file = "panel4lags.html",
                   params = list(set_title = "Panel Regressions (4 lags)", cache = FALSE, lags = 4, incl_new = FALSE))
