a10c74d230d83e485eb75969ec413e1791a6a594
timothoms <timo@geeky.net>
2021-06-30 18:05:14 -0400

    updated rendering

--- render_versions.R
+++ render_versions.R
@@ -9,9 +9,11 @@ rmarkdown::render('regressions.Rmd', params = list(cache = FALSE, optimize = TRU
 rmarkdown::render('panel.Rmd', output_file = "panel1995.html", params = list(cache = FALSE, optimize = TRUE, begin = 1995, end = 2015, incl_new = FALSE, set_title = "Panel Regressions (1995-2015)"))
 rmarkdown::render('panel.Rmd', output_file = "panel2lags.html", params = list(cache = FALSE, optimize = TRUE, lags = 2, incl_new = FALSE, set_title = "Panel Regressions (2 lags)"))
 rmarkdown::render('panel.Rmd', output_file = "panel3lags.html", params = list(cache = FALSE, optimize = TRUE, lags = 3, incl_new = FALSE, set_title = "Panel Regressions (3 lags)"))
-rmarkdown::render('panel.Rmd', params = list(cache = TRUE, optimize = TRUE))
+rmarkdown::render('panel.Rmd', params = list(cache = TRUE, optimize = FALSE))
 files <- c('index', 'summary', 'data', 'cases', 'classifications', 'sequences', 'regressions', "panel1995", "panel2lags", "panel3lags", 'panel')
 files <- paste(files, "html", sep = ".")
+files <- files[files %in% dir()]
+folders <- c("site_libs/", paste(str_replace(files, ".html", ""), "files/", sep = "_"))
 file.rename(files, paste("docs/", files, sep = ""))
-file.rename(from = "site_libs/", to = "docs/site_libs/")
+file.rename(from = folders, to = paste("docs/", folders, sep = ""))
 file.copy(from = c("links.js", "styles.css"), to = paste("docs/", c("links.js", "styles.css"), sep = ""), overwrite = TRUE, copy.date = TRUE)
